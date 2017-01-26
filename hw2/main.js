5b27c374535c46388a80bf5eeb2abcb0471a2e66
JiZhou <jz65@rice.edu>
2017-01-26 01:42:11 -0600

    comment added
    separate some functionality

--- hw2/main.js
+++ hw2/main.js
@@ -3,61 +3,72 @@
  * net id: jz65
  * student id : S01255993
  */
+
+// array of location of image files for each image position
+var images = [
+    ['./images/1.1.jpg', './images/1.2.jpg', './images/1.3.jpg', './images/1.4.jpg'],
+    ['./images/2.1.jpg', './images/2.2.jpg', './images/2.3.jpg', './images/2.4.jpg'],
+    ['./images/3.1.jpg', './images/3.2.jpg', './images/3.3.jpg', './images/3.4.jpg'],
+    ['./images/4.1.jpg', './images/4.2.jpg', './images/4.3.jpg', './images/4.4.jpg'],
+    ['./images/5.1.jpg', './images/5.2.jpg', './images/5.3.jpg', './images/5.4.jpg']
+]
+
+/* array of image item object with fields of related values:
+ * index, interval time, flag: if is stopped, element, image files,
+ * control button, number of current showing picture
+ */
+var img_items = new Array()
+var imgArr = document.getElementsByTagName('img')
+var btns = document.getElementsByClassName('card-btn')
+
+
+// a function return random between 1000 and 5000
+function newRandRate() {
+    return (Math.random()*0.8+0.2)* 5000
+}
+
 window.onload = function () {
-    var images = [
-        ['./images/1.1.jpg', './images/1.2.jpg', './images/1.3.jpg', './images/1.4.jpg'],
-        ['./images/2.1.jpg', './images/2.2.jpg', './images/2.3.jpg', './images/2.4.jpg'],
-        ['./images/3.1.jpg', './images/3.2.jpg', './images/3.3.jpg', './images/3.4.jpg'],
-        ['./images/4.1.jpg', './images/4.2.jpg', './images/4.3.jpg', './images/4.4.jpg'],
-        ['./images/5.1.jpg', './images/5.2.jpg', './images/5.3.jpg', './images/5.4.jpg']
-    ]
 
-    var img_items = new Array()
-    imgArr = document.getElementsByTagName('img')
-    var btns = document.getElementsByClassName('card-btn')
-
-    function newRandRate() {
-        return (Math.random()*0.8+0.2)* 5000
-    }
+    // initialize img_item object
     for( i=0 ; i<imgArr.length;i++){
         img_items[i]=new Object()
         img_items[i].id = i;
         img_items[i].interval = newRandRate()
-        img_items[i].is_stopped = false
         img_items[i].element = imgArr[i]
         img_items[i].images = images[i]
         img_items[i].btnDiv = btns[i]
         img_items[i].count = 0;
     }
 
+    // set interval for each image item
     img_items.forEach(function (item) {
         setIndividualInterval(item)
     })
 
+
    function setIndividualInterval(item){
-        var timer = setInterval(function () {  if(!item.is_stopped){
+        var timer = setInterval(function () {
                 console.log('image pos '+item.id+' showing pic#' + (item.count % item.images.length))
                 item.element.setAttribute('src', item.images[item.count % item.images.length])
                 item.count = item.count + 1
-            }
         }, item.interval);
         item.timer = timer
     }
 
     function stopIndividualInterval(item) {
        clearInterval(item.timer)
+        item.timer = null
     }
 
 
+    // set the onclick function for each image related button
     img_items.forEach(function (item) {
         item.btnDiv.onclick = function () {
-            if(item.is_stopped){
-                setIndividualInterval(item)
-                item.is_stopped = false
+            if(!item.timer){
                 item.interval = newRandRate()
+                setIndividualInterval(item)
                 item.btnDiv.innerHTML = "Stop"
             }else{
-                item.is_stopped = true
                 item.btnDiv.innerHTML = "Start"
                 stopIndividualInterval(item)
             }
--- hw2/profile.js
+++ hw2/profile.js
@@ -3,14 +3,114 @@
  * net id: jz65
  * student id : S01255993
  */
+
+
+
+
+// get profile item list
+var prof_items = document.getElementsByClassName("profile-item")
+
+// object profile stores the label - input - default value - alert space pairs
+var profile = new Object();
+
+// array of field and related matching regex
+var validate_reg = [
+    {id: "email", reg: "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]+$", alert: "e.g. 123@de.de"},
+    {id: "phone", reg: "[0-9]{10}", alert: "should be a ten digit number"},
+    {id: "zipcode", reg: "[0-9]{5}", alert: "should be five digits"}
+]
+
+// validation for password and confirmation password
+function validate_psw() {
+    var psw = profile['password'].input_field.value
+    var default_psw = profile['password'].default_value
+    var psw_confirm = profile['password_confirm'].input_field.value
+    if( psw == default_psw){
+        // check if new password is same as default value
+        profile['password'].alert_div.style.display = 'inline'
+        profile['password'].alert_div.innerHTML = 'password has not changed'
+        return false;
+    }
+    if(!psw && psw_confirm){
+        // check if the user input a new psw confirm but without a new psw
+        profile['password'].alert_div.style.display = 'inline'
+        profile['password'].alert_div.innerHTML = 'No new password entered'
+        return false
+    }else if(psw && !psw_confirm){
+        // check if the user input a new psw but without a new psw confirmation
+        profile['password'].alert_div.style.display = 'inline'
+        profile['password'].alert_div.innerHTML = 'Please confirm your password'
+        return false
+    }else if(psw && psw_confirm){
+        // check if psw equals psw confirmation
+        if(psw == psw_confirm){
+            return true
+        }
+    }else if(!psw && !psw_confirm){
+        // check if there is no new psw and confirmation section clear
+        return true
+    }
+    return false
+}
+
+// method for validation reduction
+function validate_val(status, item){
+    var newVal = profile[item.id].input_field.value
+    if(newVal){
+        if(!newVal.match(item.reg)){
+            profile[item.id].alert_div.innerHTML = item.alert
+        }
+        return newVal.match(item.reg)&&status
+    }
+    return status
+}
+
+/* function for update button on click
+ * 1. check if there is a new password and confirmed and equals
+ * 2. check if there are new email, phone num, or zipcode
+ * 2.1 if new, check if matched pattern regex
+ * 2.2 if one of them fails, the result is false
+ * 3. update default value part using alert field to notify user of value change
+ */
+function check_diff() {
+
+    if( validate_psw() && validate_reg.reduce(validate_val, true)) {
+
+        for(var key in profile){
+            var newVal=profile[key].input_field.value
+            if(newVal != "" && key != 'password'&& key !='password_confirm'){
+                if(profile[key].default_value != newVal){
+                    profile[key].alert_div.innerHTML = 'changed from '+ profile[key].default_value +' to '+ newVal
+                    profile[key].default_div.innerHTML = newVal
+                }else{
+                    profile[key].alert_div.innerHTML = 'no change'
+                }
+            }else if(key == 'password'  && newVal){
+
+                profile[key].alert_div.innerHTML = 'password changed successfully'
+            }
+            // update default field
+            profile[key].input_field.value = null
+            if(newVal != "" && newVal != null){
+                profile[key].default_value = newVal
+            }
+        }
+        return true
+    }
+    return false
+}
+
 window.onload = function () {
+
+
+    // get the update button
     var update_btn = document.getElementsByClassName('card-btn')[0]
+    // add listener to update button
     update_btn.addEventListener('click',check_diff,false)
 
 
-    var prof_items = document.getElementsByClassName("profile-item")
-    var profile = new Object();
-    
+
+    // look up and initialize the value for each corresponding profile item
     for( i=0; i<prof_items.length; i++){
         if (prof_items[i].getAttribute('name')) {
             profile[prof_items[i].getAttribute('name')] = {
@@ -19,68 +119,12 @@ window.onload = function () {
                 alert_div: prof_items[i].children[3],
                 input_field: (prof_items[i].children[1]).getElementsByTagName('input')[0]
             }
+
+            // define focus event for each input field
             profile[prof_items[i].getAttribute('name')].input_field.addEventListener('focus', function (item) {
                 item.currentTarget.parentNode.parentNode.children[3].innerHTML = ''
             })
         }
     }
 
-
-
-
-    function validate_psw() {
-        var psw = profile['password'].input_field.value
-        var psw_confirm = profile['password_confirm'].input_field.value
-        if(!psw.value && psw_confirm.value){
-            profile['password'].alert_div.style.display = 'inline'
-            profile['password'].alert_div.innerHTML = 'No new password entered'
-            return false
-        }else if(psw.value && !psw_confirm.value){
-            profile['password'].alert_div.style.display = 'inline'
-            profile['password'].alert_div.innerHTML = 'Please confirm your password'
-            return false
-        }else if(psw.value && psw_confirm.value){
-            if(psw.value == psw_confirm.value){
-                return true
-            }
-        }else if(!psw.value && !psw_confirm.value){
-            return true
-        }
-        return false
-    }
-
-    var validate_reg = [
-        {id: "email", reg: "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]+$", alert: "e.g. 123@de.de"},
-        {id: "phone", reg: "[0-9]{10}", alert: "should be a ten digit number"},
-        {id: "zipcode", reg: "[0-9]{5}", alert: "should be five digits"}
-    ]
-
-    function validate_val(status, item){
-        var newVal = profile[item.id].input_field.value
-        if(newVal){
-             if(!newVal.match(item.reg)){
-                 profile[item.id].alert_div.innerHTML = item.alert
-             }
-             return newVal.match(item.reg)&&status
-        }
-        return status
-    }
-    function check_diff() {
-
-       if( validate_psw() && validate_reg.reduce(validate_val, true)) {
-
-           for(var key in profile){
-               if(profile[key].input_field.value){
-                   var newVal=profile[key].input_field.value
-                   if(profile[key].default_value != profile[key].input_field.value){
-                       profile[key].alert_div.innerHTML = 'changed from '+ profile[key].default_value +' to '+ newVal
-                       profile[key].input_field.value = ''
-                       profile[key].default_div.innerHTML = newVal
-                   }
-               }
-           }
-           return true
-       }
-       return false
-    }
 }
