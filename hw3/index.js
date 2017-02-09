aca7749217df797f4899adb85443a0290925c7a2
JiZhou <jz65@rice.edu>
2017-02-08 20:45:43 -0600

    documentation

--- hw3/index.js
+++ hw3/index.js
@@ -3,17 +3,23 @@
  */
 'use strict'
 
+// the number of rows
 const N_ROW = 4
+// the number of columns
 const N_COL = 5
-const time_gap = 20000
+// the beginning time gap
+const time_gap = 18000
+// time gap decrement after each connection
 const dec = 2000
+// total number of pairs
 const pairs = N_COL*N_ROW/2
 
+// generate random in from 0 to num-1
 const ran_int = (num)=>{
     return Math.floor(Math.random()*num)
 }
 
-
+// dynamically generate html
 let make_row = (content) => [
     '<div class="col">',
     ...content,
@@ -26,6 +32,7 @@ let make_cell= ()=> [
     '</div>'
 ].join('')
 
+// function for update cookie with new score, remain time and times of try
 const updateCookie = (score, time, try_time) => {
     const key_try = 'try_count'
     const key_time = 'left_time'
@@ -50,7 +57,7 @@ const updateCookie = (score, time, try_time) => {
 
 
 }
-
+// function for showing history at first entering page
 const showCookie = ()=>{
     const key_try = 'try_count'
     const key_time = 'left_time'
@@ -65,20 +72,30 @@ const showCookie = ()=>{
                                                     + oldTime+ " seconds left with "+ oldTry + " tries."
 }
 
+// body of game
 let reset_game =()=>{
+    // timer of game
     let my_timer = {
         interval: undefined,
         remain: time_gap,
         default_gap: time_gap
     }
+    // record of selected in page
     let selected =[]
+    // array of canvas related info
     let boxes = []
+    // the number of already connected pairs in one round
     let count = 0
+    // the number of tried time
     let try_time = 0
 
+    // colors for choice
     const colors = ['#FFB6C1', '#C0C0C0', '#40E0D0']
+    // shapes for choice
     const shapes = ['round', 'rectangle', 'triangle']
+    // function for drawing different shapes
     const draw_shapes =[
+        // draw a circle
         (cell)=>{
             let canvas = cell.canvas.getContext("2d")
             canvas.fillStyle = cell.color
@@ -87,6 +104,7 @@ let reset_game =()=>{
             canvas.fill()
 
         },
+        // draw a rectangle
         (cell)=>{
             let canvas = cell.canvas.getContext("2d")
             canvas.fillStyle = cell.color
@@ -94,6 +112,7 @@ let reset_game =()=>{
             canvas.fillRect(0,0, cell.size, cell.size)
 
         },
+        // draw a triangle
         (cell)=>{
             let canvas = cell.canvas.getContext("2d")
             canvas.fillStyle = cell.color
@@ -106,6 +125,7 @@ let reset_game =()=>{
         }
     ]
 
+    // allocate shape and color of each canvas box and draw it after start is clicked
     const arrange_shape = (arr) =>{
         for(let i=0; i<arr.length;i++){
             if(!arr[i].filled){
@@ -123,7 +143,7 @@ let reset_game =()=>{
         }
     }
 
-
+    // check connection on horizontal level
     let horizon = (item1, item2)=>{
         if(item1.x == item2.x){
             let min = Math.min(item1.y, item2.y)
@@ -131,15 +151,14 @@ let reset_game =()=>{
             let between = boxes.filter((el) => {
                 return el.x == item1.x && el.y > min && el.y < max
             })
-            let connect = between.reduce((res, el) => {
-                return res && el.removed
-            }, true)
+            let connect = (between.length == 0)
 
             return connect
         }
         return false
     }
 
+    // check connection on vertical level
     let vertical = (item1, item2)=>{
         if(item1.y == item2.y){
             let min = Math.min(item1.x, item2.x)
@@ -147,14 +166,13 @@ let reset_game =()=>{
             let between = boxes.filter((el)=>{
                 return el.y == item1.y && el.x>min && el.x<max
             })
-            let connect = between.reduce((res, el)=>{
-                return res && el.removed
-            },true)
+            let connect = (between.length == 0)
             return connect
         }
         return false
     }
 
+    // check if two points could be connected with one turn
     let one_turn = (item1, item2)=>{
         const loc1 = { x : item1.x, y : item2.y}
         const loc2 = { x : item2.x, y : item1.y}
@@ -170,21 +188,29 @@ let reset_game =()=>{
         return (horizon(item1,loc1)&& vertical(item2,loc1)) || (vertical(item1,loc2) && horizon(item2, loc2))
 
     }
+
+    // check if two points could be connected with two turns
     let two_turn = (item1, item2)=>{
+        // if the are in the same line
         if(item1.x ==  item2.x){
+            // if on the border
             if(item1.x == 0 || item1.x == N_ROW-1){
                 return true;
             }
+            // if not check if there is a connection
             const arr_x= boxes.filter((el)=>{return el.y==item1.y})
             const arr_y = boxes.filter((el)=>{return el.y == item2.y})
             arr_x.reduce((res,cur)=>{
                 var item_y = arr_y.filter((el)=>{return el.x == cur.x})[0]
                 if(item_y){
+                    if(item_y.x == 0 || item_y.x == N_ROW-1) return true
                     return res || horizon(cur, item_y)
                 }
                 return res ;
             },false)
         }
+
+        // same situation for same column
         if(item1.y == item2.y){
             if(item1.y == 0 || item1.y == N_COL-1){
                 return true;
@@ -194,11 +220,15 @@ let reset_game =()=>{
             arr_x.reduce((res,cur)=>{
                 var item_y = arr_y.filter((el)=>{return el.y == cur.y})[0]
                 if(item_y){
+                    if(item_y.y == 0 || item_y.y == N_COL-1) return true
                     return res || vertical(cur, item_y)
                 }
                 return res ;
             },false)
         }
+        // if not same line nor same column
+        // use item1 as a hooker to find empty cells in the same line as item1
+        // check if these cells are connected with item2 with one turn
         const x_row = [...Array(N_COL)].map((cur, id)=>{
             return {x: item1.x, y: id}
         }).filter((el)=>{return boxes.indexOf(el)<0 && horizon(el, item1)})
@@ -209,12 +239,16 @@ let reset_game =()=>{
                 return res
             }
             else{
-                if(el.y == item2.y && (el.y == 0 || el.y == N_COL-1)){
-                    return res||two_turn(el,item2)
+                if(el.y == 0 || el.y == N_COL-1){
+                    return res||horizon({x: item2.x, y: el.y}, item2)
                 }
                 return res||one_turn(el, item2)
             }
         },false)
+
+        // if not work
+        // use item1 as a hooker to find empty cells in the same column as item1
+        // check if these cells are connected with item2 with one turn
         const x_col = [...Array(N_ROW)].map((cur, id)=>{
             return {x: id, y: item1.y}
         }).filter((el)=>{return boxes.indexOf(el)<0 && vertical(el, item1)})
@@ -225,8 +259,8 @@ let reset_game =()=>{
                 return res
             }
             else{
-                if(el.x == item2.x && (el.x==0|| el.x == N_ROW-1 )){
-                    return res||two_turn(el, item2)
+                if( el.x==0|| el.x == N_ROW-1 ){
+                    return res||vertical({x: el.x, y: item2.y}, item2)
                 }
                 return res||one_turn(el, item2)
             }
@@ -234,10 +268,14 @@ let reset_game =()=>{
 
         return row_res||col_res
     }
+
+    // check connection of two items return if connected
     let connected = (item1, item2)=>{
 
         return horizon(item1,item2) || vertical(item1,item2) || one_turn(item1,item2) || two_turn(item1, item2)
     }
+
+    // fulfill the work during checking and after checking
     const checkConnect =()=>{
         const item1 = selected[0]
         const item2 = selected[1]
@@ -264,6 +302,7 @@ let reset_game =()=>{
             remove_select()
         }
     }
+    // remove items in selected and remove their current event listener of 'click'
     const remove_select = ()=>{
         selected.forEach((item)=>{
             deselected_style(item.canvas)
@@ -273,14 +312,17 @@ let reset_game =()=>{
         selected.splice(0, selected.length)
     }
 
+    // change the style of canvas to selected
     const selected_style = (canvas)=>{
         canvas.style.border = "solid 5px"
     }
-
+    // change the style of canvas to unselected
     const deselected_style = (canvas)=>{
         canvas.style.border = ""
         canvas.disabled = true
     }
+
+    // event handler for a canvas who is selected when clicked
     function deselect_canvas(e){
         let canvas = e.currentTarget
         selected.splice(selected.indexOf(boxes.filter((el)=>{ return el.canvas == canvas})[0]),1)
@@ -289,6 +331,7 @@ let reset_game =()=>{
         canvas.addEventListener('click', select_canvas, false)
 
     }
+    // event handler for a canvas who is not selected when clicked
     function select_canvas(e){
         'use strict'
         let canvas = e.currentTarget
@@ -310,6 +353,7 @@ let reset_game =()=>{
 
     }
 
+    // draw shapes according to info in boxes
     const draw_canvas = ()=>{
         boxes = ($('.cell').map((id, el)=>{
             return {
@@ -339,12 +383,14 @@ let reset_game =()=>{
     }
 
 
+    // clear a canvas and set to default
     const clear = (el)=>{
         const c = el.getContext("2d")
         c.clearRect(0,0,el.width,el.height)
         el.style.border = ""
     }
 
+    // show the after game info
     const show_info =()=>{
         if(pairs == count){
             document.getElementById('stop_info').innerHTML = "Congratulation! You won!"
@@ -352,6 +398,8 @@ let reset_game =()=>{
             document.getElementById('stop_info').innerHTML = "You are great connected "+ count+" pairs!"
         }
     }
+
+    // is called when game is over that is all pairs are connected or time is up
     const stop = ()=>{
         clearInterval(my_timer.interval)
         my_timer.default_gap = time_gap
@@ -379,6 +427,7 @@ let reset_game =()=>{
         try_time = 0
     }
 
+    // event handler for resume button
     const resume = (e)=>{
         const button =e.currentTarget
         my_timer.interval = setInterval(()=>{
@@ -394,9 +443,21 @@ let reset_game =()=>{
         }, 1000)
         button.innerHTML = "Pause"
         button.removeEventListener('click', resume, false)
-        button.addEventListener('click', pause, false)
+        button.addEventListener('click', pause, false);
+        ($('.cell').toArray()).filter((canvas)=>{
+            return canvas.style.border == "solid 5px"
+        }).forEach((canvas)=>{
+            canvas.addEventListener('click', deselect_canvas, false)
+        });
+        ($('.cell').toArray()).filter((canvas)=>{
+            return canvas.style.border == ""
+        }).forEach((canvas)=>{
+            canvas.addEventListener('click', select_canvas, false)
+        })
     }
 
+    // reset timer of game with new time gap
+    // if user connecting is fast enough than the time would be like not running away
     const reset_interval = () =>{
         my_timer.remain = Math.max(my_timer.default_gap,my_timer.remain)
         my_timer.default_gap = my_timer.default_gap>=my_timer.remain? my_timer.default_gap - dec: my_timer.default_gap
@@ -417,13 +478,20 @@ let reset_game =()=>{
     }
 
 
+    // event handler for pause button
     const pause = (e)=>{
         const button =e.currentTarget
         button.innerHTML = "Resume"
         clearInterval(my_timer.interval)
         button.removeEventListener('click', pause, false)
-        button.addEventListener('click', resume, false)
+        button.addEventListener('click', resume, false);
+        ($('.cell').toArray()).forEach((canvas)=>{
+            canvas.removeEventListener('click', select_canvas, false)
+            canvas.removeEventListener('click', deselect_canvas, false)
+        })
     }
+
+    // start the game and reset all
     const start = ()=>{
         document.getElementById('stop_info').innerHTML = ""
         const button = document.getElementById('start')
